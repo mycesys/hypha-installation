29c8ee99ff1857a8daa61e43641eb0d78bd88751
Artem Sereda <overmind88@gmail.com>
2025-11-13 02:05:35 +0300

    Fix typo

--- allinone/add-ssl-keys-to-jdk.sh
+++ allinone/add-ssl-keys-to-jdk.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-PASSHPRASE=$(cat ssl/ssl_passwords)
+PASSPHRASE=$(cat ssl/ssl_passwords)
 
 # Add SSL key to hub-auth applicatiom
 docker exec -i hub-auth bash -c "cd /ssl; keytool -delete -storepass changeit -keystore /opt/java/openjdk/lib/security/cacerts -alias hypha"
@@ -17,7 +17,7 @@ openssl pkcs12 -export -passin pass:${PASSPHRASE} -passout pass:${PASSPHRASE} -i
 cp selfsigned/options-ssl-nginx.conf ssl/options-ssl-nginx.conf
 
 #Add private key password to nginx configuration file
-# echo "$PASSPHRASE" >> ssl/ssl_passwords
+echo "$PASSPHRASE" > ssl/ssl_passwords
 echo "ssl_password_file /etc/ssl/ssl_passwords;" >> ssl/options-ssl-nginx.conf
 
 docker restart hub-auth hypha-gateway
