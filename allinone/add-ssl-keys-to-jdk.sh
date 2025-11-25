f224def994dc90afd29b4d98cd050f87de57025b
Artem Sereda <overmind88@gmail.com>
2025-11-25 21:05:22 +0300

    2025.3 Release
    2025.3 Release

--- allinone/add-ssl-keys-to-jdk.sh
+++ allinone/add-ssl-keys-to-jdk.sh
@@ -1,11 +1,23 @@
 #!/bin/bash
 
-PASSPHRASE=mycesys_ssl_pass
+PASSPHRASE=$(cat ssl/ssl_passwords)
 
 # Add SSL key to hub-auth applicatiom
-docker exec -i hub-auth bash -c "cd /ssl; keytool -importkeystore -srckeystore ./identity.p12 -srcstoretype PKCS12 -srcstorepass $PASSPHRASE -destkeystore /opt/java/openjdk/lib/security/cacerts -deststoretype JKS -deststorepass changeit"
+docker exec -i hub-auth bash -c "cd /ssl; keytool -delete -storepass changeit -keystore /opt/java/openjdk/lib/security/cacerts -alias hypha"
+docker exec -i hub-auth bash -c "cd /ssl; keytool -importkeystore -srckeystore ./identity.p12 -srcstoretype PKCS12 -srcstorepass $PASSPHRASE -destkeystore /opt/java/openjdk/lib/security/cacerts -deststoretype PKCS12 -deststorepass changeit"
 
 # Add SSL key to hypha-gateway application
-docker exec -i hypha-gateway bash -c "cd /ssl; keytool -importkeystore -srckeystore ./identity.p12 -srcstoretype PKCS12 -srcstorepass $PASSPHRASE -destkeystore /opt/java/openjdk/lib/security/cacerts -deststoretype JKS -deststorepass changeit"
+docker exec -i hypha-gateway bash -c "cd /ssl; keytool -delete -storepass changeit -keystore /opt/java/openjdk/lib/security/cacerts -alias hypha"
+docker exec -i hypha-gateway bash -c "cd /ssl; keytool -importkeystore -srckeystore ./identity.p12 -srcstoretype PKCS12 -srcstorepass $PASSPHRASE -destkeystore /opt/java/openjdk/lib/security/cacerts -deststoretype PKCS12 -deststorepass changeit"
+
+#Create pkcs12 keystore with certificate
+openssl pkcs12 -export -passin pass:${PASSPHRASE} -passout pass:${PASSPHRASE} -in ssl/fullchain.pem -inkey ssl/privkey.pem -out ssl/identity.p12 -name "hypha"
+
+#Copy nginx SSL options
+cp selfsigned/options-ssl-nginx.conf ssl/options-ssl-nginx.conf
+
+#Add private key password to nginx configuration file
+echo "$PASSPHRASE" > ssl/ssl_passwords
+echo "ssl_password_file /etc/ssl/ssl_passwords;" >> ssl/options-ssl-nginx.conf
 
 docker restart hub-auth hypha-gateway
--- vault/vault_config/common/add-secrets.sh
+++ vault/vault_config/common/add-secrets.sh
@@ -1,37 +1,60 @@
-#! /bin/bash
+#!/bin/sh
+export VAULT_ADDR='http://127.0.0.1:8201'
+export VAULT_SKIP_VERIFY='true'
+rootToken=$(grep "Initial Root Token: " < /data/generated_keys.txt  | cut -c21- )
+export VAULT_TOKEN="${rootToken}"
 
 ls /config/product | grep ".*" | \
 while read -r productName ; do
 
-   ls /config/product/$productName | grep ".*" | \
-   while read -r serviceName ; do
-     echo $FIRST_INITIALIZATION
-      if [ ! -z "$FIRST_INITIALIZATION" ]; then
-        vault kv put secret/$serviceName key=value
-      fi
+  ls /config/product/"$productName" | grep ".*" | \
+    while read -r serviceName ; do
+      echo "Processing service: $serviceName"
 
-      cat /config/product/$productName/$serviceName  | \
+      # Create tempfile
+      temp_file=$(mktemp)
+
+      # Get all KVs
+      cat /config/product/"$productName"/"$serviceName" | \
       while read -r kvString ; do
         if [ ! -z "$kvString" ]; then
-          resovledKv=$(eval echo -E "$kvString")
-
-          nonblankValueTest=$(eval echo $resovledKv | grep ".*=[\s]*$")
+          resolvedKv=$(eval echo -E "$kvString")
+          nonblankValueTest=$(echo "$resolvedKv" | grep ".*=[\s]*$")
           if [ -z "$nonblankValueTest" ]; then
-#            echo secret/$serviceName $resovledKv
-            key=$(echo $resovledKv | sed -E  's/([^=]*)=(.*)/\1/')
-            value=$(echo $resovledKv | sed -E  's/([^=]*)=(.*)/\2/')
-
-            existingValue=$(vault kv get -field=$key secret/$serviceName)
-
-            if [ "$value" != "$existingValue" ]; then
-              vault kv patch secret/$serviceName $resovledKv
-            fi
+            echo "$resolvedKv" >> "$temp_file"
           fi
         fi
       done
 
-   done
+      # Create or update KVs
+      if [ -s "$temp_file" ]; then
+        # Create if not exist
+        if ! vault kv get secret/"$serviceName" > /dev/null 2>&1; then
+          echo "Creating new secret for $serviceName"
+          # Create first KV for servcice
+          first_kv=$(head -1 "$temp_file")
+          vault kv put secret/"$serviceName" "$first_kv"
 
+          # Add another KV for service
+          tail -n +2 "$temp_file" | \
+          while read -r kvString; do
+            vault kv patch secret/"$serviceName" "$kvString"
+          done
+        else
+          echo "Updating existing secret for $serviceName"
+          cat "$temp_file" | \
+          while read -r kvString; do
+            key=$(echo "$kvString" | sed -E 's/([^=]*)=(.*)/\1/')
+            value=$(echo "$kvString" | sed -E 's/([^=]*)=(.*)/\2/')
+            existingValue=$(vault kv get -field="$key" secret/"$serviceName" 2>/dev/null || echo "NOT_EXIST")
+
+            if [ "$value" != "$existingValue" ]; then
+              vault kv patch secret/"$serviceName" "$kvString"
+            fi
+          done
+        fi
+      fi
+
+      rm -f "$temp_file"
+    done
 done
-
-
